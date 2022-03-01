a6764fd043312ed4926213d30ede6886b9ec5ed3
Andres Gama <gama_pipe@hotmail.com>
2022-03-01 17:24:18 -0500

    fix-modedark navbarFooter

--- src/components/NavBarFooter.jsx
+++ src/components/NavBarFooter.jsx
@@ -4,7 +4,7 @@ const NavBarFooter = () => {
   return (
     <nav
       id="navFooter"
-      className="flex w-full md:hidden fixed bottom-0 transition-transform duration-500 px-4 py-2 border-t z-10 bg-base border-bordes"
+      className="bg-base dark:bg-baseDark flex w-full sm:hidden fixed bottom-0 transition-transform duration-500 px-4 py-2 border-t z-10  border-bordes"
     >
       <ListOfLinks ViewInPhone={true} />
     </nav>
