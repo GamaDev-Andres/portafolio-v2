b9692dfe2639528308d37962e37c3dbf083110cd
Andres Gama <gama_pipe@hotmail.com>
2022-03-30 17:18:42 -0500

    agregando metatags y descarga de cv

--- src/components/home/sectionPresentation/ListOfLinksContact.jsx
+++ src/components/home/sectionPresentation/ListOfLinksContact.jsx
@@ -1,11 +1,12 @@
+import cv from '../../../assets/pdf/Andres-Gama-CV-Fullstack-developer.pdf'
+
 const ListOfLinksContact = () => {
   return (
     <div className="py-4 gap-4 flex justify-start items-center flex-wrap">
       <a
-        target="_blank"
+        download="Andres-Gama-CV-Fullstack-developer.pdf"
         className=" text-sm border border-solid shadow-blue-200 shadow-lg border-blue-400 px-6 py-2 rounded-full"
-        href="https://drive.google.com/file/d/1IIUutqCGZL_KSN7NqE-JdRYQl_6Qaqhd/view"
-        rel="noreferrer"
+        href={cv}
       >
         Resumé
       </a>
